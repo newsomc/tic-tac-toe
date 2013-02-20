ece5d81f3e180668baf75c9d8f1974958d031fa0
Clint Newsom <hcnewsom@gmail.com>
2013-02-20 15:17:05 -0500

    Adding atoms to manage state, plus a new key to game-state which counts moves made

--- src-cljs/tic-tac-toe/cljs.cljs
+++ src-cljs/tic-tac-toe/cljs.cljs
@@ -1,6 +1,7 @@
 (ns tic-tac-toe.core
 	(:use [jayq.core :only [$ css inner find data text current-target on click html]]
-	 	  [jayq.util :only [log clj->js]]))
+        [jayq.util :only [log clj->js]])
+  (:require [clojure.string :as string]))
 
 ;;------------------
 ;; Logging
@@ -10,7 +11,7 @@
  (.log js/console (pr-str my-var)))
 
 ;;------------------
-;; DOM Elements
+;; Vars 
 ;;------------------
 
 (def $turn-prompt ($ :#TurnPrompt))
@@ -20,18 +21,19 @@
 (def $td ($ :td))
 (def $body ($ :body))
 
-;;------------------
-;; Vars TODO: Use an atom to designate synchronously modified values. 
-;;------------------
+(defn new-game-state
+  "Returns an atom with a map which holds the entire state of the game."
+  []
+   (atom {:turns []
+          :finished false
+          :winner false
+          :draw false
+          :active-player false}))
 
-(def turns (array))
-(def finished false)
-(def winner false)
-(def draw false)
-(def active-player false)
+(def game-state (new-game-state))
 
 ;;------------------
-;; App
+;; Helpers
 ;;------------------
 
 (defn set-history 
@@ -49,14 +51,20 @@
 	[]
 	(.modal $turn-prompt (clj->js {:show :true})))
 
-(defn cache-board 
-	"Create a map of the current table state."
+(defn make-board 
+	"Create a map representation of the current table state."
 	[table]
-	(let [rows {}]
-		(into rows 
-			(for [x (range 3) 
-			      y (range 3)] 
-			      [(str x y) (.find table (str "[data-x=" x "][data-y=" y "]"))]))))
+  (into {} 
+    (for [x (range 3) 
+          y (range 3)] 
+      [(str x y) (.find table (str "[data-x=" x "][data-y=" y "]"))])))
+
+(defn update-game
+  "Updates the current game state. If we are adding to the turns update-in and
+   conj the value onto vector. If we are chaning a symbol variable lets assoc."
+  [key value]
+  (if (= key :turns) (swap! game-state update-in [:turns] conj value)
+      (swap! game-state assoc-in [key] value)))
 
 ;; ----------------------
 ;; Rules
@@ -67,26 +75,24 @@
 	[selector]
 	(.data ($ selector) "player"))
 
-(defn check-axis-fn
+(defn check-axis
   "Higher order function to check axis."
-	[axis-fn]
-	(fn [axis player rows]
-		(every? identity (map #(== player (player-data ((axis-fn axis %) rows)))
+	[axis? axis player rows]
+  (let [axis-fn (if (= axis? :x)
+                  #(str %1 %2)
+                  #(str %2 %1))]
+    (every? identity (map #(== player (player-data ((axis-fn axis %) rows)))
                           ["0" "1" "2"]))))
-
-(def check-row-x (check-axis-fn #(str %1 %2)))
-(def check-row-y (check-axis-fn #(str %2 %1)))
 
 (defn check-all-diags 
 	"Check for matching moves along each diagonal."
 	[player rows]
-	(or (and (== (.data ($ ("00" rows)) "player") player) 
-           (== (.data ($ ("11" rows)) "player") player) 
-           (== (.data ($ ("22" rows)) "player") player)) 
-
-	    (and (== (.data ($ ("02" rows)) "player") player) 
-           (== (.data ($ ("11" rows)) "player") player) 
-           (== (.data ($ ("20" rows)) "player") player))))
+  (letfn [(check [row-key]
+            (== player (player-data (row-key rows))))
+          (all-same [row-keys]
+            (every? identity (map check row-keys)))]
+    (or (all-same ["00" "11" "22"])
+        (all-same ["02" "11" "20"]))))
 
 (defn check-diags 
 	"If the sum of x and y are odd, we're not on a diagonal."
@@ -98,16 +104,15 @@
 	"Conditions for game end."
 	[x y player rows]
 	(or
-    (check-row-y y player rows) 
-		(check-row-x x player rows) 
-		(check-diags x y player rows)
-    ))
+    (check-axis :y y player rows) 
+		(check-axis :x x player rows) 
+		(check-diags x y player rows)))
 
 (defn set-winner 
 	"Conditions for winning game. Uses set! which is a bit gnarly I admit."
 	[player]
-	(set! winner player)
-	(set! finished true)
+  (update-game :winner player)
+  (update-game :finished true)
 	(.text $status-text (+ (str player) " Won"))
 	(.css $game-table "background" "#FAFAFA"))
 
@@ -121,20 +126,20 @@
         x (apply str (get turn 0))
         y (apply str (get turn 1))
         row-key (+ (str x) (str y))
-        rows (cache-board $game-table)
+        rows (make-board $game-table)
         winner ()]
-      (.text ($ (row-key rows)) player)
-		  (.data ($ (row-key rows)) (clj->js {:player player}))
-		  (if (game-over x y player rows)
-		  	(set-winner player))))
+    (.text ($ (row-key rows)) player)
+    (.data ($ (row-key rows)) (clj->js {:player player}))
+    (if (game-over x y player rows)
+      (set-winner player))))
 
 (defn check-draw 
 	"If turns are greater than or equal to 9 and no winner, draw is declared."
 	[turns]
-	(if (and (>= (alength turns) 9) (= false winner))
+	(if (and (>= (count (@game-state :turns)) 9) (== false (@game-state :winner)))
 		(do
-			;;(set! finished true)
-			(set! draw true)
+      (update-game :finished true)
+      (update-game :draw true)
 			(.text $status-text "Draw!")
 			(.css  $game-table "background" "#FAFAFA"))))
 
@@ -142,61 +147,58 @@
 	"Sets current turn."
 	[aplayer]
 	(.text $status-text (apply str (+ aplayer "'s Turn")))
-	(set! active-player aplayer))
+  (update-game :active-player aplayer))
 
 (defn route 
 	"Builds turn array based on path."
 	[]
 	(let [path (.substring (.-pathname (aget js/window "location")) 5)
-		  turn (.slice path -1)
-		  turns (.split (.slice path 1 -2) "-")]
-		  (if (empty? turn) (start-game))
-		  (set-turn turn)
-		  (if-not (empty? turns)
-		  	(do (play turn turns)
-		  	(check-draw turns)))))
+        turn (.slice path -1)
+        turns (.split (.slice path 1 -2) "-")]
+    (if (empty? turn) (start-game))
+    (set-turn turn)
+    (if-not (empty? turns)
+      (do (play turn turns)
+          (check-draw turns)))))
 
 (defn construct-path 
 	"Create the location path."
 	[]
-	(apply str(+ "/game/" (.join turns "-") "/" active-player)))
+	(apply str(+ "/game/" (string/join "-" (@game-state :turns)) "/" (@game-state :active-player))))
 
 (defn change-turn []
-	(if (== active-player "x") (set-turn "o") (set-turn "x")))
+  ;;  (console-log (clj->js @game-state))
+	(if (== (@game-state :active-player) "x") (set-turn "o") (set-turn "x")))
 
 (defn select-turn 
 	"Starts the game when selecting a player from modal window."
 	[e]	
 	(let [elem ($ (curr-target e)) 
-		  turn (.data elem "turn")
-		  url ( apply str (+ "/game/" turn))]
+        turn (.data elem "turn")
+        url (str (+ "/game/" turn))]
 		(.modal $turn-prompt (clj->js "hide"))
 		(set-turn turn)
 		(set-history url)))
 
 (defn take-turn 
-	"Create the current turn. Check if cells are occupied. 
-	 TODO: change checking for td text to player data with jQuery."
+	"Create the current turn. Check if cells are occupied."
 	[e]
 	(let [cell ($ (curr-target e)) 
         x (.data cell "x")
         y (.data cell "y")
-        empty-cell "&nbsp;"
-        row-key (+ (str y) (str y))
-        rows (cache-board $game-table)]
+        row-key (+ (str y) (str y))]
     (if-not (== (.data cell) nil )
       (do
-           (console-log turns)
-           (.push turns (+ x "," y))		  
-           (change-turn)
-           (set-history (construct-path))
-           (route)))))
+        (swap! game-state update-in [:turn-count] (fnil inc 0))
+        (update-game :turns (+ x "," y))
+        (change-turn)
+        (set-history (construct-path))
+        (route)))))
 
 (defn init 
-	"Initialize the app!"
+	"Initialize the app"
 	[]
 	(.modal $turn-prompt (clj->js {:show :false}))
-	(cache-board $game-table)
 	(.addEventListener js/window "popstate" (route))
 	(.click $button (fn [e] (select-turn e)))
 	(.click ($ (.find $game-table "td"))(fn [e] (take-turn e))))
